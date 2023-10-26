41e75a974f8ee6b35326a85e189a6a1b03746d71
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-10-26 16:31:20 -0300

    Finalização da Minimal API - ApiTarefa

--- ApiTarefa/Program.cs
+++ ApiTarefa/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("TarefasDb"));
+
+//var connectionString = builder.Configuration.GetConnectionString("DefualtConnection") ??
+//    throw new InvalidOperationException("Connection string 'DefualtConnection' not found.");
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -16,8 +27,74 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Olá Mundo");
 
-app.MapGet("frases", async () =>
+app.MapGet("frasesAletorias", async () =>
 await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes"));
+
+app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) =>
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+app.MapGet("/tarefas/concluida", async (AppDbContext db) =>
+    await db.Tarefas.Where(p => p.IsConcluida).ToListAsync());
+
+app.MapGet("/tarefa", async (AppDbContext db) =>
+{
+    return await db.Tarefas.AsNoTracking().ToListAsync();   
+});
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/tarefas/{tarefa.TarefaId}", tarefa);
+
+});
+
+app.MapPut("/tarefa/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+
+    if (tarefa is null)
+    {
+        return Results.NotFound();
+    }
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluida = inputTarefa.IsConcluida;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+
+        return Results.Ok(tarefa);
+    }
+
+    return Results.NoContent();
+});
 
 app.Run();
 
+class Tarefa
+{
+    public int TarefaId { get; set; }
+    public string Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
